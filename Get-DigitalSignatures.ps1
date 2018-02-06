dc40404010fef8360dec870401935445c0ce59e6
Aaron Parker <aaron@stealthpuppy.com>
2018-02-06 14:43:39 +1100

    Logic updates; Support multiple input
    Logic updates, error checking, time to complete; Support multiple input

--- Get-DigitalSignatures.ps1
+++ Get-DigitalSignatures.ps1
@@ -1,120 +1,145 @@
-<#
-    .SYNOPSIS
-        Get digital signatures from files in a target folder.
-    
-    .DESCRIPTION
-        Gets digital signatures from .exe and .dll files from a specified path and sub-folders.
-        Retreives the certificate thumbprint, certificate name, certificate expiry, certificate validity and file path and outputs the results.
-        Output includes files that are not signed.
-
-    .NOTES
-        Name: Get-DigitalSignatures.ps1
-        Author: Aaron Parker
-        Twitter: @stealthpuppy
-    
-    .LINK
-        http://stealthpuppy.com
-
-    .OUTPUT
-        [System.Array]
-
-    .PARAMETER Path
-        A target path in which to scan files for digital signatures.
-
-    .PARAMETER OutPath
-        A target path to export certificates in P7B file format to. Each file will be named for the certificte thumbprint.
-
-    .PARAMETER Unique
-        By default the script will return all files and their certificate details. Use -Unique to return the first listing for each unique certificate.
-
-    .PARAMETER Gridivew
-        The script will return an object that can be used on the pipeline; however, use -Gridview output directly to an interactive table in a separate window.
-
-    .EXAMPLE
-        .\Get-DigitalSignatures.ps1 -Path "C:\Users\aaron\AppData\Local\GitHubDesktop"
-
-        Description:
-        Scans the folder specified in the Path variable and returns the digital signatures for each file.
-
-    .EXAMPLE
-        .\Get-DigitalSignatures.ps1 -Path "C:\Users\aaron\AppData\Local\GitHubDesktop" -OutPath C:\Temp
-
-        Description:
-        Scans the folder specified in the Path variable and returns the digital signatures for each file.
-        A .P7B certificate file will be exported for each unique certificate and stored in the C:\Temp folder
-
-    .EXAMPLE
-        .\Get-DigitalSignatures.ps1 -Path "C:\Users\aaron\AppData\Local\GitHubDesktop" -Unique
-
-        Description:
-        Scans the folder specified in the Path variable and returns the digital signatures for only the first file with a unique certificate.
-#>
 Function Get-DigitalSignatures {
-    [CmdletBinding(SupportsShouldProcess = $False, ConfirmImpact = "Low", DefaultParameterSetName='Base')]
+    <#
+        .SYNOPSIS
+            Get digital signatures from files in a target folder.
+        
+        .DESCRIPTION
+            Gets digital signatures from .exe and .dll files from a specified path and sub-folders.
+            Retreives the certificate thumbprint, certificate name, certificate expiry, certificate validity and file path and outputs the results.
+            Output includes files that are not signed.
+
+        .NOTES
+            Name: Get-DigitalSignatures.ps1
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+        
+        .LINK
+            http://stealthpuppy.com
+
+        .OUTPUT
+            [System.Array]
+
+        .PARAMETER Path
+            A target path in which to scan files for digital signatures.
+
+        .PARAMETER OutPath
+            A target path to export certificates in P7B file format to. Each file will be named for the certificte thumbprint.
+
+        .PARAMETER Unique
+            By default the script will return all files and their certificate details. Use -Unique to return the first listing for each unique certificate.
+
+        .PARAMETER Gridivew
+            The script will return an object that can be used on the pipeline; however, use -Gridview output directly to an interactive table in a separate window.
+
+        .EXAMPLE
+            .\Get-DigitalSignatures.ps1 -Path "C:\Users\aaron\AppData\Local\GitHubDesktop"
+
+            Description:
+            Scans the folder specified in the Path variable and returns the digital signatures for each file.
+
+        .EXAMPLE
+            .\Get-DigitalSignatures.ps1 -Path "C:\Users\aaron\AppData\Local\GitHubDesktop" -OutPath C:\Temp
+
+            Description:
+            Scans the folder specified in the Path variable and returns the digital signatures for each file.
+            A .P7B certificate file will be exported for each unique certificate and stored in the C:\Temp folder
+
+        .EXAMPLE
+            .\Get-DigitalSignatures.ps1 -Path "C:\Users\aaron\AppData\Local\GitHubDesktop" -Unique
+
+            Description:
+            Scans the folder specified in the Path variable and returns the digital signatures for only the first file with a unique certificate.
+    #>
+    [CmdletBinding(SupportsShouldProcess=$False, DefaultParameterSetName='Base')]
     Param (
-        [Parameter(ParameterSetName='Base', Mandatory=$False, ValueFromPipeline=$True, HelpMessage='Specify a target path in which to scan files for digital signatures.')]
-        [ValidateScript({ If (Test-Path $_ -PathType 'Container') { $True } Else { Throw "Cannot find path $_" } })]
+        [Parameter(ParameterSetName='Base', Mandatory=$False, ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$True, `
+        HelpMessage='Specify a target path in which to scan files for digital signatures.')]
+        # [ValidateScript({ If (Test-Path $_ -PathType 'Container') { $True } Else { Throw "Cannot find path $_" } })]
+        [Alias('FullName','PSPath')]
         [string[]]$Path = ".\",
 
         [Parameter(ParameterSetName='Base', Mandatory=$False, HelpMessage='Output certificates to files in a specific folder.')]
-        [ValidateScript({ If (Test-Path $_ -PathType 'Container') { $True } Else { Throw "Cannot find path $_" } })]
+        [ValidateScript({ If (Test-Path $_ -PathType 'Container') { $True } Else { Throw "Cannot find output path $_" } })]
         [string]$OutPath,
 
         [Parameter(ParameterSetName='Base', Mandatory=$False, HelpMessage='Specify the records to return - all records, or unique thumbprints.')]
-        [switch]$Unique = $False,
+        [switch]$Unique,
 
         [Parameter(ParameterSetName='Base', Mandatory=$False, HelpMessage='Enable output to a Grid View.')]
-        [switch]$Gridview = $False
+        [switch]$Gridview
     )
 
-    Function Export-P7bFile {
-        Param (
-            [string]$File,
-            [string]$OutPath
-        )
-        $cert = (Get-AuthenticodeSignature $File).SignerCertificate
-        Write-Verbose "Exporting certificate: $OutPath\$($cert.Thumbprint).p7b"
-        Export-Certificate -Cert $cert -FilePath "$OutPath\$($cert.Thumbprint).p7b" -Type P7B
+    Begin {
+        # Measure time taken to gather data
+        $StopWatch = [system.diagnostics.stopwatch]::StartNew()
+        Function Export-P7bFile {
+            Param (
+                [string]$File,
+                [string]$OutPath
+            )
+            $cert = (Get-AuthenticodeSignature $File).SignerCertificate
+            Write-Verbose "Exporting certificate: $OutPath\$($cert.Thumbprint).p7b"
+            Export-Certificate -Cert $cert -FilePath "$OutPath\$($cert.Thumbprint).p7b" -Type P7B
+        }
+        
+        # Initialise $Signatures as an array
+        $Signatures = @()
     }
+    Process {
+        # For each path in $Path, check that the path exists
+        If (Test-Path -Path $Path -IsValid) {
 
-    $Signatures = @()
-    # If multiple items passed in via $Path, process each item
-    ForEach ( $item in $Path ) {
-        # Get Exe and Dll files from the target path (inc. subfolders), find signatures and return certain properties in a grid view
-        Write-Verbose "Getting digital signatures for: $item"
-        $Signatures += Get-ChildItem -Path $item -Recurse -Include '*.exe', '*.dll' | `
-                    Get-AuthenticodeSignature | `
-                    Select-Object @{Name = "Thumbprint"; Expression = {$_.SignerCertificate.Thumbprint}}, `
-                            @{Name = "Subject"; Expression = {$_.SignerCertificate.Subject}}, `
-                            @{Name = "Expiry"; Expression = {$_.SignerCertificate.NotAfter}}, `
-                            Status, `
-                            Path | `
-                    Sort-Object -Property Thumbprint
+            # Get the item to determine whether it's a file or folder
+            If ((Get-Item -Path $Path).PSIsContainer) {
+
+                # Target is a folder, so trawl the folder for .exe and .dll files in the target and sub-folders
+                Write-Verbose "Scanning files in folder: $Path"
+                $items = Get-ChildItem -Path $Path -Recurse -Include '*.exe', '*.dll'
+            } Else {
+
+                # Target is a file, so just get metadata for the file
+                Write-Verbose "Scanning file: $Path"
+                $items = Get-ChildItem -Path $Path
+            }
+
+            # Get Exe and Dll files from the target path (inc. subfolders), find signatures and return certain properties in a grid view
+            Write-Verbose "Getting digital signatures for: $Path"
+            $Signatures += $items | Get-AuthenticodeSignature | `
+                        Select-Object @{Name = "Thumbprint"; Expression = {$_.SignerCertificate.Thumbprint}}, `
+                                @{Name = "Subject"; Expression = {$_.SignerCertificate.Subject}}, `
+                                @{Name = "Expiry"; Expression = {$_.SignerCertificate.NotAfter}}, `
+                                Status, `
+                                Path | `
+                        Sort-Object -Property Thumbprint
+
+        } Else {
+                Write-Error "Path does not exist: $Path"
+        }
     }
-    
-    # If $OutPath specified we only want to return one file per certificate
-    If ($OutPath) { $Unique = $True }
+    End {
+        # If -Unique is specified, filter the signatures list and return the first item of each unique certificate
+        If ($Outpath -or $Unique) { 
+            Write-Verbose "Filtering for unique signatures."
+            $Signatures = $Signatures | Where-Object {$_.Status -eq "Valid" -or $_.Status -eq "UnknownError" } | `
+                Group-Object -Property Thumbprint | `
+                ForEach-Object { $_.Group | Select-Object -First 1 }
+            Write-Verbose "$($Signatures.Count) unique signature/s found in $Path"
+        }
 
-    # If -Unique is specified, filter the signatures list and return the first item of each unique certificate
-    If ($Unique) { 
-        Write-Verbose "Filtering for unique signatures."
-        $Signatures = $Signatures | Where-Object {$_.Status -eq "Valid" -or $_.Status -eq "UnknownError" } | `
-            Group-Object -Property Thumbprint | `
-            ForEach-Object { $_.Group | Select-Object -First 1 }
-        Write-Verbose "$($Signatures.Count) unique signature/s found."
+        # Output the a P7b certificate file for each unique certificate found from files in the folder
+        If ($OutPath) {
+            Write-Verbose "Exporting certificate P7B files to $Outpath."
+            ForEach ( $file in $Signatures.Path ) {
+                Export-P7bFile -File $file -OutPath $OutPath | Out-Null
+            } 
+        }
+
+        # If Gridview switch specified, output to a Grid View
+        If ($Gridview) { $Signatures | Out-GridView -Title "Digital Signatures: $Path" }
+
+        # Return output
+        $StopWatch.Stop()
+        Write-Verbose "Digital signature trawling complete. Script took $($StopWatch.Elapsed.TotalMilliseconds) ms to complete."
+        Return $Signatures
     }
-
-    # Output the a P7b certificate file for each unique certificate found from files in the folder
-    If ($OutPath) {
-        Write-Verbose "Exporting signatures to $Outpath."
-        ForEach ( $file in $Signatures.Path ) {
-            Export-P7bFile -File $file -OutPath $OutPath | Out-Null
-        } 
-    }
-
-    # If Gridview switch specified, output to a Grid View
-    If ($Gridview) { $Signatures | Out-GridView -Title "Digital Signatures: $Path" }
-
-    # Return output
-    Return $Signatures
 }
--- Get-FileMetadata.ps1
+++ Get-FileMetadata.ps1
@@ -1,87 +1,83 @@
-<#
-    .SYNOPSIS
-        Get file metadata from files in a target folder.
-    
-    .DESCRIPTION
-        Retreives file metadata from files in a target path, or file paths, to display information on the target files.
-        Useful for understanding application files and identifying metadata stored in them. Enables the administrator to view metadata for application control scenarios.
-
-    .NOTES
-        Name: Get-FileMetadata.ps1
-        Author: Aaron Parker
-        Twitter: @stealthpuppy
-    
-    .LINK
-        http://stealthpuppy.com
-
-    .OUTPUT
-        [System.Array]
-
-    .PARAMETER Path
-        A target path in which to scan files for metadata.
-
-    .EXAMPLE
-        .\Get-FileMetadata.ps1 -Path "C:\Users\aaron\AppData\Local\GitHubDesktop"
-
-        Description:
-        Scans the folder specified in the Path variable and returns the metadata for each file.
-#>
 Function Get-FileMetadata {
+    <#
+        .SYNOPSIS
+            Get file metadata from files in a target folder.
+        
+        .DESCRIPTION
+            Retreives file metadata from files in a target path, or file paths, to display information on the target files.
+            Useful for understanding application files and identifying metadata stored in them. Enables the administrator to view metadata for application control scenarios.
+
+        .NOTES
+            Name: Get-FileMetadata.ps1
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+        
+        .LINK
+            http://stealthpuppy.com
+
+        .OUTPUT
+            [System.Array]
+
+        .PARAMETER Path
+            A target path in which to scan files for metadata.
+
+        .EXAMPLE
+            .\Get-FileMetadata.ps1 -Path "C:\Users\aaron\AppData\Local\GitHubDesktop"
+
+            Description:
+            Scans the folder specified in the Path variable and returns the metadata for each file.
+    #>
     [CmdletBinding(SupportsShouldProcess=$False)]
     Param (
         [Parameter(Mandatory=$False, ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$True, `
         HelpMessage='Specify a target path, paths or a list of files to scan for metadata.')]
+        # [ValidateScript({ If ((Get-Item -Path $_).PSProvider.Name -ne 'FileSystem') { $True } Else { Throw "$_ is not a file system path." } })]
         [Alias('FullName','PSPath')]
         [string[]]$Path = ".\"
     )
 
     Begin {
+        # Measure time taken to gather data
+        $StopWatch = [system.diagnostics.stopwatch]::StartNew()
+
         Write-Verbose "Beginning metadata trawling."
         $Files = @()
     }
     Process {
+        # For each path in $Path, check that the path exists
+        If (Test-Path -Path $Path -IsValid) {
 
-        # If multiple items passed in via $Path, process each item
-        ForEach ( $item in $Path ) {
+            # Get the item to determine whether it's a file or folder
+            If ((Get-Item -Path $Path).PSIsContainer) {
 
-            # Check that the path exists
-            If (Test-Path $item) {
-
-                # Get the item to determine whether it's a file or folder
-                $target = Get-Item -Path $item
-                If ($target.PSIsContainer) {
-
-                    # Target is a folder, so trawl the folder for .exe and .dll files in the target and sub-folders
-                    Write-Verbose "Getting metadata for folder: $target"
-                    $Files += Get-ChildItem -Path $target -Recurse -Include '*.exe', '*.dll' | `
-                    Select-Object @{Name = "Path"; Expression = {$_.FullName}}, `
-                            @{Name = "Description"; Expression = {$_.VersionInfo.FileDescription}}, `
-                            @{Name = "Product"; Expression = {$_.VersionInfo.ProductName}}, `
-                            @{Name = "Company"; Expression = {$_.VersionInfo.CompanyName}}, `
-                            @{Name = "FileVersion"; Expression = {$_.VersionInfo.FileVersion}}, `
-                            @{Name = "ProductVersion"; Expression = {$_.VersionInfo.ProductVersion}} | `
-                    Sort-Object -Property Path
-                } Else {
-
-                    # Target is a file, so just get metadata for the file
-                    Write-Verbose "Getting metadata for file: $target"
-                    $Files += Get-ChildItem -Path $target | `
-                    Select-Object @{Name = "Path"; Expression = {$_.FullName}}, `
-                        @{Name = "Description"; Expression = {$_.VersionInfo.FileDescription}}, `
-                        @{Name = "Product"; Expression = {$_.VersionInfo.ProductName}}, `
-                        @{Name = "Company"; Expression = {$_.VersionInfo.CompanyName}}, `
-                        @{Name = "FileVersion"; Expression = {$_.VersionInfo.FileVersion}}, `
-                        @{Name = "ProductVersion"; Expression = {$_.VersionInfo.ProductVersion}} | `
-                    Sort-Object -Property Path
-                }
+                # Target is a folder, so trawl the folder for .exe and .dll files in the target and sub-folders
+                Write-Verbose "Getting metadata for files in folder: $Path"
+                $items = Get-ChildItem -Path $Path -Recurse -Include '*.exe', '*.dll'
             } Else {
-                Write-Error "Path does not exist: $item"
+
+                # Target is a file, so just get metadata for the file
+                Write-Verbose "Getting metadata for file: $Path"
+                $items = Get-ChildItem -Path $Path
             }
+
+            # Create an array from what was returned for specific data and sort on file path
+            $Files += $items | Select-Object @{Name = "Path"; Expression = {$_.FullName}}, `
+                @{Name = "Description"; Expression = {$_.VersionInfo.FileDescription}}, `
+                @{Name = "Product"; Expression = {$_.VersionInfo.ProductName}}, `
+                @{Name = "Company"; Expression = {$_.VersionInfo.CompanyName}}, `
+                @{Name = "FileVersion"; Expression = {$_.VersionInfo.FileVersion}}, `
+                @{Name = "ProductVersion"; Expression = {$_.VersionInfo.ProductVersion}} | `
+                Sort-Object -Property Path
+
+        } Else {
+                Write-Error "Path does not exist: $Path"
         }
     }
     End {
 
         # Return the array of file paths and metadata
+        $StopWatch.Stop()
+        Write-Verbose "Metadata trawling complete. Script took $($StopWatch.Elapsed.TotalMilliseconds) ms to complete."
         Return $Files
     }
 }
