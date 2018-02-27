91abe2afd1750c795b62f54788ea69b210b84e06
Aaron Parker <aaron@stealthpuppy.com>
2018-02-27 22:38:55 +1100

    Fix function names

--- Invoke-Configuration.ps1
+++ Invoke-Configuration.ps1
@@ -3,7 +3,7 @@ Import-Module ApplicationControl -Verbose
 
 $Folder = "C:\Users\aaron\AppData\Local\Microsoft\Teams", "C:\Users\aaron\AppData\Local\Microsoft\TeamsMeetingAddin"
 
-$Certs = Get-DigitalSignatures -Path $Folder -Unique -Verbose
-$Unsigned = Get-DigitalSignatures.ps1 -Path $Folder -Verbose | Where-Object { $_.Status -eq "NotSigned" }
-$Unsigned = Get-FileMetadata.ps1 -Verbose -Path $Unsigned.Path
-New-AampConfiguration.ps1 -AccessibleFiles $Unsigned -AccessibleFolders $Certs -SignedFiles $Certs -Verbose
+$Certs = Get-AcDigitalSignature -Path $Folder -Unique -Verbose
+$Unsigned = Get-AcDigitalSignature -Path $Folder -Verbose | Where-Object { $_.Status -eq "NotSigned" }
+$Unsigned = Get-AcFileMetadata -Verbose -Path $Unsigned.Path
+New-AcAampConfiguration -AccessibleFiles $Unsigned -AccessibleFolders $Certs -SignedFiles $Certs -Verbose
