9c199cd7c17c49602ee25a1cd50a9a26bd396e29
Aaron Parker <aaron@stealthpuppy.com>
2018-03-26 20:09:51 +1100

    appyeyor.yml and Install.ps1 regression

--- tests/Install.ps1
+++ tests/Install.ps1
@@ -2,25 +2,14 @@
     .SYNOPSIS
         AppVeyor tests setup script.
 #>
-[string[]]$PowerShellModules = @("Pester", "posh-git", "PSScriptAnalyzer")
-[string[]]$PackageProviders = @('NuGet', 'PowerShellGet')
-
 # Line break for readability in AppVeyor console
 Write-Host -Object ''
 Write-Host "PowerShell Version:" $PSVersionTable.PSVersion.tostring()
 
-# Install the PowerShell Modules
-ForEach ($Module in $PowerShellModules) {
-    Write-Verbose "Installing module: $($Module)"
-    Install-Module $Module -Scope CurrentUser -Force -Repository PSGallery
-    Import-Module $Module -Force
-}
-
-# Install package providers for PowerShell Modules
-ForEach ($Provider in $PackageProviders) {
-    Write-Verbose "Installing provider: $($Provider)"
-    Install-PackageProvider $Provider -Force -ForceBootstrap -Scope CurrentUser
-}
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name Pester -Force
+Install-Module -Name PSScriptAnalyzer -Force
+Install-Module -Name posh-git -Force
 
 # Import the ApplicationControl module
 If (Test-Path 'env:APPVEYOR_BUILD_FOLDER') {
